631b0b9371ce7f5f9e3add49a0ab457f240f8261
Kahindo Lwanzo Alfred <89693650+alfredlw@users.noreply.github.com>
2022-08-08 18:35:04 +0200

    theme color #FFCB08

---
+++ .history/pages/_document_20220808183347.tsx
@@ -0,0 +1,24 @@
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+
+type Props = {};
+
+class MyDocument extends NextDocument<Props> {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <meta name="theme-color" content="#EF6C0D" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
