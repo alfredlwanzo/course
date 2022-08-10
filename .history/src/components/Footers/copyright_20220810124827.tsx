abc5b55d68ae71cf51cce263883bfaf89e0fa80b
Kahindo Lwanzo Alfred <89693650+alfredlw@users.noreply.github.com>
2022-08-10 12:50:36 +0200

    somechanges ..........

--- .history/src/components/Footers/copyright_20220810124827.tsx
+++ .history/src/components/Footers/copyright_20220810124827.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Copyright = () => {
   return (
     <div className='max-w-7xl mx-auto px-3 sm:px-6" py-3 text-gray-200 '>
-        <p className='text-xs text-center md:text-left'>© 2022 Innov Toast. Tous droits réservés.</p>
+        <p className='text-xs text-center'>© 2022 Innov Toast. Tous droits réservés.</p>
     </div>
   )
 }
